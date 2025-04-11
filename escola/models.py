1fe9fb3bd3eb2c170352199f445db11ba02e349c
Rafaellegend <Rafaellegendofdark@hotmail.com>
2025-04-11 15:25:48 -0300

    creating 'Matriculas' Model, View, Serializer and Urls

--- escola/models.py
+++ escola/models.py
@@ -1,19 +1,16 @@
 from django.db import models
 
 '''
-Id
-Nome
-
-E-mail
-Não pode estar em branco
-
-CPF
-Máximo de 11 caracteres
-
-Data de Nascimento
-
-Número de Celular
-Máximo de 14 caracteres
+Modelo Estudante
+    - Id
+    - Nome
+    - E-mail
+        - Não pode estar em branco
+    - CPF
+        - Máximo de 11 caracteres
+    - Data de Nascimento
+    - Número de Celular
+        - Máximo de 14 caracteres
 '''
 class Estudante(models.Model):
     nome = models.CharField(max_length=100)
@@ -26,18 +23,16 @@ class Estudante(models.Model):
         return self.nome
     
 '''
-Id
-
-Código
-Máximo de 10 caracteres
-
-Descrição
-Não pode estar em Branco
-
-Nível (Básico, Intermediário e Avançado)
-Não pode estar em Branco
-Não pode ser Nulo
-Por padrão deve ser Básico
+Model Curso
+    - Id
+    - Código
+        - Máximo de 10 caracteres
+    - Descrição
+        - Não pode estar em Branco
+    - Nível (Básico, Intermediário e Avançado)
+        - Não pode estar em Branco
+        - Não pode ser Nulo
+        - Por padrão deve ser Básico
 '''
 class Curso(models.Model):
     NIVEL = (
@@ -52,3 +47,25 @@ class Curso(models.Model):
 
     def __str__(self):
         return self.codigo
+
+'''
+Modelo Matricula
+    - Id
+    - Relacionar com o modelo Estudante
+        - Se o Estudante for deletado, todas as matrículas daquele estudante devem ser deletadas
+    - Relacionar com o modelo Curso
+        - Se o Curso for deletado, todas as matriculas daquele curso devem ser deletadas
+    - Período (Matutino, Vespertino, Noturno)
+        - Não pode estar em Branco
+        - Não pode ser Nulo
+        - Por padrão deve ser Matutino
+'''
+class Matricula(models.Model):
+    PERIODO = (
+        ('M', 'Matutino'),
+        ('V', 'Vespertino'),
+        ('N', 'Noturno'),
+    )
+    estudante = models.ForeignKey(Estudante, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    periodo = models.CharField(max_length=1, choices=PERIODO, blank=False, null=False, default= 'M')
