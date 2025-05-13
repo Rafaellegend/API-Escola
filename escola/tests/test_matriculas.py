fe3d83fd6966856ad7c77932143c816ec6284d4e
Rafaellegend <Rafaellegendofdark@hotmail.com>
2025-05-12 22:49:46 -0300

    setting request tests for "matriculas"

---
+++ escola/tests/test_matriculas.py
@@ -0,0 +1,62 @@
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
+from escola.models import Estudante, Curso, Matricula
+
+class MatriculasTestCase(APITestCase):
+    def setUp(self):
+        self.usuario = User.objects.create_superuser(
+            username='admin',
+            password='admin123'
+        )
+        self.url = reverse('Matriculas-list')
+        self.client.force_authenticate(user=self.usuario)
+        self.estudante = Estudante.objects.create(
+            nome = 'Teste estudante Um',
+            email = 'Testeestudante01@gmail.com',
+            cpf = '37926838063',
+            data_nascimento = '2023-02-02',
+            celular = '86 99999-9999'
+        )
+        self.curso = Curso.objects.create(
+            codigo = 'TST1',
+            descricao = 'Teste Curso Um',
+            nivel = 'B'
+            )
+        self.matricula = Matricula.objects.create(
+            estudante=self.estudante,
+            curso=self.curso,
+            periodo='M'
+        )
+
+    def test_requisicao_get_para_listar_matriculas(self):
+        """Teste que verifica uma requisição GET para listar matriculas."""
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_requisicao_post_para_criar_matricula(self):
+        """Teste que verifica uma requisição POST para criar um matricula."""
+        dados = {
+            'estudante': self.estudante.pk,
+            'curso': self.curso.pk,
+            'periodo': 'M'
+        }
+        response = self.client.post(self.url, dados)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_requisicao_delete_uma_matricula(self):
+        """Teste que verifica uma requisição DELETE para criar um matricula."""
+        response = self.client.delete(f'{self.url}2/')
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+    
+
+    def test_requisicao_put_para_atualizar_um_matricula(self):
+        """Teste de requisição PUT para atualizar uma matricula."""
+        dados = {
+            'estudante': self.estudante.pk,
+            'curso': self.curso.pk,
+            'periodo': 'V'
+        }
+        response = self.client.put(f'{self.url}1/', data=dados)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
