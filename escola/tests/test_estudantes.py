4019c5dacdd273e0184c83b9e2bb84e45ccdbdb2
Rafaellegend <Rafaellegendofdark@hotmail.com>
2025-05-12 22:49:33 -0300

    setting request tests for "estudantes"

---
+++ escola/tests/test_estudantes.py
@@ -0,0 +1,71 @@
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
+from escola.models import Estudante
+from escola.serializers import EstudanteSerializer
+
+class EstudantesTestCase(APITestCase):
+    def setUp(self):
+        self.usuario = User.objects.create_superuser(
+            username='admin',
+            password='admin123'
+        )
+        self.url = reverse('Estudantes-list')
+        self.client.force_authenticate(user=self.usuario)
+        self.estudante_01 = Estudante.objects.create(
+            nome = 'Teste estudante Um',
+            email = 'Testeestudante01@gmail.com',
+            cpf = '37926838063',
+            data_nascimento = '2023-02-02',
+            celular = '86 99999-9999'
+        )
+        self.estudante_02 = Estudante.objects.create(
+            nome = 'Teste estudante Dois',
+            email = 'Testeestudante02@gmail.com',
+            cpf = '19111128054',
+            data_nascimento = '2004-03-27',
+            celular = '81 98547-7674'
+        )
+
+    def test_requisicao_get_para_listar_estudantes(self):
+        """Teste que verifica uma requisição GET para listar estudantes."""
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_requisicao_get_para_listar_Um_estudante(self):
+        """Teste que verifica uma requisição GET para listar um estudante."""
+        response = self.client.get(self.url+'1/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        dados_estudante = Estudante.objects.get(pk=1)
+        dados_estudante_serializados = EstudanteSerializer(instance=dados_estudante).data
+        self.assertEqual(response.data, dados_estudante_serializados)
+
+    def test_requisicao_post_para_criar_estudante(self):
+        """Teste que verifica uma requisição POST para criar um estudante."""
+        dados = {
+            'nome': 'Teste',
+            'email': 'teste@gmail.com',
+            'cpf': '68531672015',
+            'data_nascimento': '1969-02-27',
+            'celular': '65 99145-0773'
+        }
+        response = self.client.post(self.url, data=dados)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_requisicao_delete_um_estudante(self):
+        """Teste de requisição DELETE para deletar um estudante."""
+        response = self.client.delete(f'{self.url}2/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_requisicao_put_para_atualizar_um_estudante(self):
+        """Teste de requisição PUT para atualizar um estudante."""
+        dados = {
+            'nome': 'Teste',
+            'email': 'testeput@gmail.com',
+            'cpf': '24869327040',
+            'data_nascimento': '1969-02-27',
+            'celular': '65 99145-0737'
+        }
+        response = self.client.put(f'{self.url}1/', data=dados)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
